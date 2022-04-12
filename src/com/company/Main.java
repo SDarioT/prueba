36cf4d8c5d116637512b72abecb4232467a6ed95
dario_t <stenezaca@est.ups.edu.ec>
2022-04-12 08:32:32 -0500

    12/04/22

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+   // string placa, cedula;
+
+    public static void main(String[] args) {
+        // write your code here
+       // System.out.println("hola");
+        //var read = (new Scanner(System.in)).);
+        //' read.nexline() ' es para una cadena con espacios
+        Double numero;
+        int i = 0;
+        var read = new Scanner(System.in);
+
+        while ( i < 10) {
+
+            System.out.println("Ingrese numero; ");
+            numero = read.nextDouble();
+            i=i+1;
+        }
+    }
+
+}
